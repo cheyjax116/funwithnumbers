fd3cfe861511f7e927005c493287ac3608514783
Crystal Word <crystalword@Crystals-iMac.local>
2021-02-22 19:35:35 -0500

    commiting initial files for numbers API page

---
+++ api.js
@@ -0,0 +1,12 @@
+
+document.querySelector("button").addEventListener("click", fetchNumbers)
+
+async function fetchNumbers(){
+
+const response = await fetch('http://numbersapi.com/random/trivia')
+const data = await response.text();
+document.querySelector(".par").innerHTML = data;
+document.querySelector(".par").classList.add('box');
+}
+
+
