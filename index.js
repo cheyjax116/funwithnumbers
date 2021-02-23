065446f580c231281c79da0c9666852535218cf1
Crystal Word <crystalword@Crystals-iMac.local>
2021-02-22 21:05:03 -0500

    returned to http designation

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.querySelector("button").addEventListener("click", fetchNumbers)
 
 async function fetchNumbers(){
 
-const response = await fetch('https://numbersapi.com/random/trivia')
+const response = await fetch('http://numbersapi.com/random/trivia')
 const data = await response.text();
 document.querySelector(".par").innerHTML = data;
 document.querySelector(".par").classList.add('box');
